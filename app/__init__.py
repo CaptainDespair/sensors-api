0b306df608bd6e066465d862eb824b581840873c
CaptainDespair <danila.syrokvasov@yandex.ru>
2023-04-26 11:29:23 +0300

    changed method GET, exc+

--- app/__init__.py
+++ app/__init__.py
@@ -2,7 +2,7 @@ from flask import Flask
 from flask_migrate import Migrate
 
 from app.models import db, Event, Sensor
-from app.config import DevelopementConfig
+from app.config import ProductiontConfig, DevelopementConfig
 
 app = Flask(__name__)
 app.config.from_object(DevelopementConfig)
--- app/views.py
+++ app/views.py
@@ -21,9 +21,14 @@ def show_all_events():
 
 @app.route('/all_events/<int:id>')
 def events_pagination(id):
-    event = Event.query.get(id)
-    return render_template('event_info.html', 
-                           event=event)
+    try:
+        event = Event.query.get(id)
+        return render_template('event_info.html', 
+                            event=event)
+    except:
+        error = 'Ошибка базы данных'
+        return render_template('event_info.html',
+                               error=error)
 
 
 @app.route('/all_sensors')
@@ -38,49 +43,55 @@ def show_all_sensors():
 
 @app.route('/all_sensors/<int:id>')
 def get_events_from_sensor(id):
-    sensor = Sensor.query.get(id)
-    events = Event.query.\
-                   filter_by\
-                   (sensor_id = id)\
-                   .all()
-    return render_template('get_sensor_events.html', 
-                           events=events, 
-                           sensor=sensor)
+    try:
+        sensor = Sensor.query.get(id)
+        events = Event.query.\
+                    filter_by\
+                    (sensor_id = id)\
+                    .all()
+        return render_template('get_sensor_events.html', 
+                            events=events, 
+                            sensor=sensor)
+    except:
+        error = 'Ошибка базы данных'
+        return render_template('get_sensor_events.html',
+                               error=error)
 
 
-@app.route('/filter')
+@app.route('/filter', methods=["GET", "POST"])
 def filter():
     try:
-        temperature = request.args.get('temperature')
-        humidity = request.args.get('humidity')
-        if temperature and humidity:
-            filtered_events = Event\
-                              .query\
-                              .filter(Event.temperature == temperature or\
-                                      Event.humidity == humidity)\
-                              .all()
-            return render_template('filtered_events.html',\
-                                    events=filtered_events,\
-                                    temperature=temperature,\
-                                    humidity=humidity)
-        if temperature:
-            filtered_events = Event\
-                              .query\
-                              .filter(Event.temperature == temperature)\
-                              .all()
-            return render_template('filtered_events.html',\
-                                    events=filtered_events,\
-                                    temperature=temperature,\
-                                    humidity=humidity)
-        if humidity:
-            filtered_events = Event\
-                              .query\
-                              .filter(Event.humidity == humidity)\
-                              .all()
-            return render_template('filtered_events.html',\
-                                    events=filtered_events,\
-                                    temperature=temperature,\
-                                    humidity=humidity)
+        if request.method == 'POST':
+            temperature = request.form.get('temperature')
+            humidity = request.form.get('humidity')
+            if temperature and humidity:
+                filtered_events = Event\
+                                .query\
+                                .filter(Event.temperature == temperature or\
+                                        Event.humidity == humidity)\
+                                .all()
+                return render_template('filtered_events.html',\
+                                        events=filtered_events,\
+                                        temperature=temperature,\
+                                        humidity=humidity)
+            if temperature:
+                filtered_events = Event\
+                                .query\
+                                .filter(Event.temperature == temperature)\
+                                .all()
+                return render_template('filtered_events.html',\
+                                        events=filtered_events,\
+                                        temperature=temperature,\
+                                        humidity=humidity)
+            if humidity:
+                filtered_events = Event\
+                                .query\
+                                .filter(Event.humidity == humidity)\
+                                .all()
+                return render_template('filtered_events.html',\
+                                        events=filtered_events,\
+                                        temperature=temperature,\
+                                        humidity=humidity)
     except:
         error = 'Вы неправильно ввели значения. Введите значения типа INT'
         return render_template('filter.html', error=error)

cb7ffa415941c14985c74d3c078f98ef6d5641bc
CaptainDespair <danila.syrokvasov@yandex.ru>
2023-04-26 16:00:16 +0300

    Create, Delete, Read for all models ADDED.

--- app/views.py
+++ app/views.py
@@ -1,6 +1,6 @@
-from flask import request
-from flask import render_template
-from sqlalchemy.exc import IntegrityError, DataError, DatabaseError
+from flask import request, render_template, redirect
+from sqlalchemy.exc import IntegrityError, PendingRollbackError
+
 
 from app import app, db
 from app.models import Event, Sensor
@@ -20,12 +20,23 @@ def show_all_events():
         return render_template('index.html')
     
 
-@app.route('/all_events/<int:id>')
-def events_pagination(id):
+@app.route('/all_events/<int:id>', methods=["GET", "POST"])
+def get_event_info(id):
     try:
-        event = Event.query.get(id)
-        return render_template('event_info.html', 
-                            event=event)
+        if request.method == "GET":
+            event = Event.query.get(id)
+            return render_template('event_info.html', 
+                                event=event)
+        if request.method == 'POST':
+            event_id = request.form.get('delete_event')
+            event = Event.query.get(event_id)
+            db.session.delete(event)
+            db.session.commit()
+            done_message = 'Событие удалено'
+            return render_template('all_events.html',\
+                                   done_message=done_message)
+        else:
+            return render_template('all_events.html')
     except:
         error = 'Ошибка базы данных'
         return render_template('event_info.html',
@@ -42,22 +53,35 @@ def show_all_sensors():
         return render_template('index.html')
 
 
-@app.route('/all_sensors/<int:id>')
+@app.route('/all_sensors/<int:id>', methods=["GET", "POST"])
 def get_events_from_sensor(id):
     try:
-        sensor = Sensor.query.get(id)
-        events = Event.query.\
-                    filter_by\
-                    (sensor_id = id)\
-                    .all()
-        return render_template('get_sensor_events.html', 
-                            events=events, 
-                            sensor=sensor)
+        if request.method == 'GET':
+            sensor = Sensor.query.get(id)
+            events = Event.query.\
+                        filter_by\
+                        (sensor_id = id)\
+                        .all()
+            return render_template('get_sensor_events.html', 
+                                events=events, 
+                                sensor=sensor)
+        if request.method == 'POST':
+            sensor_id = request.form.get('delete_sensor')
+            sensor = Sensor.query.get(sensor_id)
+            db.session.delete(sensor)
+            db.session.commit()
+            done_message = 'Датчик удален'
+            return render_template('all_sensors.html',\
+                                   done_message=done_message)
+        else:
+            return render_template('all_sensors.html')
     except:
         error = 'Ошибка базы данных'
         return render_template('get_sensor_events.html',
                                error=error)
+    
 
+   
 
 @app.route('/filter', methods=["GET", "POST"])
 def filter():
@@ -112,7 +136,7 @@ def create_sensor():
                 success_message = f'Успешно добавлен сенсор {name}'
                 return render_template('create_sensor.html',\
                                         success_message=success_message)
-        except(IntegrityError, DataError, Exception) as error:
+        except(IntegrityError, Exception) as error:
             if error == IntegrityError:
                 get_max_id = Sensor\
                                 .query\
@@ -133,3 +157,50 @@ def create_sensor():
                                        annotation=annotation)
         else:
             return render_template('create_sensor.html')
+        
+
+@app.route('/create_event', methods=["GET", "POST"])
+def create_event():
+    try:
+        if request.method == 'POST':
+            sensor_id = request.form.get('sensor_id')
+            name = request.form.get('name')
+            temperature = request.form.get('temperature')
+            humidity = request.form.get('humidity')
+            new_event = Event(sensor_id=sensor_id,\
+                                name=name,\
+                                temperature=temperature,\
+                                humidity=humidity)
+            db.session.add(new_event)
+            db.session.commit()
+            success_message = f'Успешно добавлено событие {name}'
+            return render_template('create_event.html',\
+                                    success_message=success_message)
+    except(IntegrityError, PendingRollbackError, Exception) as error:
+        if error == IntegrityError:
+            get_max_id = Event\
+                            .query\
+                            .order_by(Event.id.desc())\
+                            .first()
+            print('----------------')
+            print(get_max_id.id)
+            max_id = get_max_id.id + 1
+            new_event = Event(id=max_id,\
+                                sensor_id=sensor_id,\
+                                name=name,\
+                                temperature=temperature,\
+                                humidity=humidity)
+            db.session.add(new_event)
+            db.session.commit()
+            success_message = f'Успешно добавлено событие {name}'
+            return render_template('create_event.html',\
+                                    success_message=success_message)
+        else:
+            error_message = 'Ошибка базы данных.' 
+            annotation = f'"sensor id": id существующего датчика,\
+                        \n"название": String, "температура": INT, "влажность": INT'
+            return render_template('create_event.html',\
+                                    error_message=error_message,
+                                    annotation=annotation)
+    else:
+        return render_template('create_event.html')
